c9b7d39113e897ca44842c2d422356d700db3d56
User <User@192.168.1.14>
2022-11-23 18:51:47 +0530

    Javadocs

---
+++ JavaDocs/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"documents","l":"Multiplication"}];updateSearchResults();
---
+++ JavaDocs/src/documents/Multiplication.java
@@ -0,0 +1,42 @@
+package documents;
+
+/**
+ * Date:3-nov-2022
+ * This class is a simple multiply of two integers
+ * @author pavan
+ * @version 1.8
+ *
+ */
+public class Multiplication {
+	/**
+	 * integer first value 
+	 */
+	public int a;
+	/**
+	 * integer second value
+	 */
+	public int b;
+
+	/**
+	 * this method multiples a and b and returns the integer result
+	 * @param a first integer value
+	 * @param b second integer value
+	 * @return multiplication of a and b
+	 */
+	/**
+	 * @param a
+	 * @param b
+	 * @return
+	 */
+	public int mul(int a,int b) {
+		return a*b;
+	}
+	/**
+	 * Main method of multiplication class
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	//	System.out.println(sum);
+	}
+
+}
